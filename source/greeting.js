122330653b3db8ded9479d46ee2c395efaeb20a6
Adam T. Croft <sendtoatc@gmail.com>
2019-01-01 17:53:41 -0800

    Greeting is now time-based

--- source/greeting.js
+++ source/greeting.js
@@ -1,6 +1,14 @@
 function writeGreeting() {
   let greeting = document.getElementById("greeting");
-  greeting.innerHTML = "Hello, Adam";
+  let time = new Date();
+
+  if (time.getHours() < 12) {
+    greeting.innerHTML = "Good Morning, Adam";
+  } else if (time.getHours() > 11 && time.getHours() < 18) {
+    greeting.innerHTML = "Good Afternoon, Adam";
+  } else {
+    greeting.innerHTML = "Good Evening, Adam";
+  }
 }
 
 writeGreeting();
